1a1201fb970daf041daf5dd07511ee950deb4d12
Lex Toumbourou <lex@canva.com>
2025-10-25 16:11:26 +1000

    Add remaining EfficientNet models.

--- src/mlxim/model/efficientnet/__init__.py
+++ src/mlxim/model/efficientnet/__init__.py
@@ -1,5 +1,5 @@
 from ._blocks import MBConv, MBConvConfig
-from ._factory import efficientnet_b0, efficientnet_configs
+from ._factory import efficientnet_b0, efficientnet_b1, efficientnet_b2, efficientnet_b3, efficientnet_b4, efficientnet_b5, efficientnet_b6, efficientnet_b7, efficientnet_configs
 from .efficientnet import EfficientNet
 
 __all__ = [
@@ -7,14 +7,35 @@ __all__ = [
     "MBConv",
     "MBConvConfig",
     "efficientnet_b0",
+    "efficientnet_b1",
+    "efficientnet_b2",
+    "efficientnet_b3",
+    "efficientnet_b4",
+    "efficientnet_b5",
+    "efficientnet_b6",
+    "efficientnet_b7",
     "EFFICIENTNET_ENTRYPOINT",
     "EFFICIENTNET_CONFIG",
 ]
 
 EFFICIENTNET_ENTRYPOINT = {
     "efficientnet_b0": efficientnet_b0,
+    "efficientnet_b1": efficientnet_b1,
+    "efficientnet_b2": efficientnet_b2,
+    "efficientnet_b3": efficientnet_b3,
+    "efficientnet_b4": efficientnet_b4,
+    "efficientnet_b5": efficientnet_b5,
+    "efficientnet_b6": efficientnet_b6,
+    "efficientnet_b7": efficientnet_b7,
 }
 
 EFFICIENTNET_CONFIG = {
     "efficientnet_b0": efficientnet_configs["efficientnet_b0"],
+    "efficientnet_b1": efficientnet_configs["efficientnet_b1"],
+    "efficientnet_b2": efficientnet_configs["efficientnet_b2"],
+    "efficientnet_b3": efficientnet_configs["efficientnet_b3"],
+    "efficientnet_b4": efficientnet_configs["efficientnet_b4"],
+    "efficientnet_b5": efficientnet_configs["efficientnet_b5"],
+    "efficientnet_b6": efficientnet_configs["efficientnet_b6"],
+    "efficientnet_b7": efficientnet_configs["efficientnet_b7"],
 }
